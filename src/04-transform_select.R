6234c71dd62233f4be0b4fbebaebdba4fe31083a
julian-urbano <urbano.julian@gmail.com>
2018-04-24 17:27:40 +0200

    Add 03-margins_select and 04-transform_select.

---
+++ src/04-transform_select.R
@@ -0,0 +1,37 @@
+source("src/common.R")
+source("src/io.R")
+
+for(measure in .MEASURES) {
+  for(collection in .COLLECTIONS) {
+    cat("\n", collection, measure); flush.console()
+
+    in.path <- file.path("scratch/02-transform/", paste0(collection, "_", measure))
+    out1.path <- file.path("scratch/04-transform_select/", paste0(collection, "_", measure))
+    out2.path <- file.path("output/04-transform_select/")
+    dir.create(out1.path, recursive = TRUE, showWarnings = FALSE)
+    dir.create(out2.path, recursive = TRUE, showWarnings = FALSE)
+
+    dat <- read.csv(paste0("data/", collection, "_", measure, ".csv"))
+    fits <- matrix("", ncol = 3, nrow = ncol(dat), dimnames = list(NULL, c("logLik", "AIC", "BIC")))
+
+    for(i in 1:nrow(fits)) {
+      sysname <- colnames(dat)[i]
+      set.seed(i)
+
+      eff.files <- list.files(in.path, pattern = paste0("^", sysname, "_.+$"), full.names = TRUE)
+      if(length(eff.files) > 0) { # where transformation didn't fail
+        effs <- lapply(eff.files, load.object)
+        effs <- sample(effs) # shuffle to minimize biases due to order
+        for(method in colnames(fits)) {
+          eff <- effSelect(effs, method = method)
+          fits[i, method] <- eff$model$type
+
+          save.object(eff, file.path(out1.path, method, sysname))
+        }
+      }
+    }
+
+    write.csv(file = file.path(out2.path, paste0(collection, "_", measure, ".csv")),
+              row.names = FALSE, fits)
+  }
+}
